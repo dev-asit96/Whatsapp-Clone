9fd5494f96dd6b743474b8ce0f6f882bd88f71be
asit96-dev <asitdebata96@gmail.com>
2020-06-19 11:17:36 +0530

    Login Activity

--- app/src/main/java/com/asit/chatoo/LoginActivity.java
+++ app/src/main/java/com/asit/chatoo/LoginActivity.java
@@ -2,9 +2,9 @@ package com.asit.chatoo;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -23,6 +23,7 @@ public class LoginActivity extends AppCompatActivity {
 
     MaterialEditText email, password;
     Button btn_login;
+    private ProgressBar progressbar;
 
     FirebaseAuth auth;
 
@@ -40,31 +41,34 @@ public class LoginActivity extends AppCompatActivity {
         email = findViewById(R.id.email);
         password = findViewById(R.id.password);
 
+        progressbar = findViewById(R.id.progressBar);
         btn_login = findViewById(R.id.btn_login);
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 String txt_username = Objects.requireNonNull(email.getText()).toString();
                 String txt_password = Objects.requireNonNull(password.getText()).toString();
 
-                if (TextUtils.isEmpty(txt_username) || TextUtils.isEmpty(txt_password)) {
-                    Toast.makeText(LoginActivity.this, "All Fields required!", Toast.LENGTH_LONG).show();
+//                if (TextUtils.isEmpty(txt_username) || TextUtils.isEmpty(txt_password)) {
+                //Toast.makeText(LoginActivity.this, "All Fields required!", Toast.LENGTH_LONG).show();
+                auth.signInWithEmailAndPassword(txt_username, txt_password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        progressbar.setVisibility(View.VISIBLE);
+                        if (task.isSuccessful()) {
+                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                            startActivity(intent);
+                            finish();
+                            progressbar.setVisibility(View.GONE);
+                        } else
+                            Toast.makeText(LoginActivity.this, "Authentication Failed!", Toast.LENGTH_SHORT).show();
 
-                    auth.signInWithEmailAndPassword(txt_username, txt_password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (task.isSuccessful()) {
-                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-                                startActivity(intent);
-                                finish();
-                            } else {
-                                Toast.makeText(LoginActivity.this, "Authentication Failed!", Toast.LENGTH_SHORT).show();
-                            }
-                        }
-                    });
-                }
+                    }
+                });
             }
+
         });
     }
 }
